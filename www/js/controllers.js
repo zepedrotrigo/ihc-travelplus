97285a08b5a26393dcd0183ddeae98212dce263e
José Trigo <josetrigo@ua.pt>
2021-05-25 19:41:10 +0100

    se estiver logged out faz redirect, apaguei os titulos que estavam bugados, impedir de fazer go back

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,20 +16,22 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('myFlightsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('myFlightsCtrl', ['$scope', '$stateParams', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
+function ($scope, $stateParams, $state) {
+    if (localStorage['loggedIn'] === "false") {
+        $state.go('tabsController.login');
+    }
 }])
       
-.controller('profileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('profileCtrl', ['$scope', '$stateParams', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
+    function ($scope, $stateParams, $state) {
+        if (localStorage['loggedIn'] === "false") {
+            $state.go('tabsController.login');
+        }
 }])
    
 .controller('searchFlightCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
